ca5c1e250e195413b895c32ac50b3d1a4f475c99
esopp <esoppenheim@gmail.com>
2020-06-29 22:18:43 -0400

    make user belongs_to user_joinable association in user model optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :user_rubrics
   has_many :user_joins
-  belongs_to :user_joinable, polymorphic: true
+  belongs_to :user_joinable, polymorphic: true, optional: true
 
   has_many :plans, through: :user_joins, source: :user_joinable, source_type: 'Plan'
   has_many :plan_aspects, through: :user_joins, source: :user_joinable, source_type: 'PlanAspect'
